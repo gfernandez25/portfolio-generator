e606f09fa0860d8d74cbff6526e635f570d294c8
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-08-07 16:50:59 -0400

    added inquirer

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/*
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+*/
+
+
+
+
+
+/* using just node js
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
@@ -13,6 +43,7 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
 
+*/
 
 
 /*
