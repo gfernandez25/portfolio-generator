37353a8e0dac3de58f61e7987fdf034922d75780
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-08-07 15:12:52 -0400

    Generate the HTML File

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+/*
 
 var profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
@@ -15,4 +33,14 @@ const printProfileData = profileDataArr => {
 
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+
+
+/*
+string/format literals
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    GitHub: ${githubName}
+  `;
+};*/
